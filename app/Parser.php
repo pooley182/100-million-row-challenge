496972f8f4fd67ed7bb2f5792d8419760c195e9c
David Pooley <dave@pooley-mail.co.uk>
2026-02-25 19:58:33 +0000

    single threaded parsing

--- app/Parser.php
+++ app/Parser.php
@@ -8,6 +8,38 @@ final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $fileHandle = fopen($inputPath, 'rb');
+        
+        stream_set_read_buffer($fileHandle, 1 << 27); // 128MB buffer
+        gc_disable();
+
+        $output = [];
+
+        while(($line = fgets($fileHandle)) !== false) {
+            $comma = strpos($line, ',');
+
+            $path = substr($line, 19, $comma - 19);
+            $date = substr($line, $comma + 1, 10);
+            
+            if (!isset($output[$path][$date])){
+                $output[$path][$date] = 1;
+            } else {
+                $output[$path][$date]++;
+            }
+        }
+
+        fclose($fileHandle);
+        gc_enable();
+
+        foreach ($output as &$dates) {
+            if (count($dates) > 1) {
+                ksort($dates, SORT_STRING);
+            }
+        }
+        unset($dates);
+
+        $json = json_encode($output, JSON_PRETTY_PRINT);
+        file_put_contents($outputPath, $json);
+        
     }
 }
