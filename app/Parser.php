2bb8e69d52c06ae17c2f84f55e3287199165b026
David Pooley <dpooley@wonde.com>
2026-02-27 15:31:19 +0000

    swap fgets for fread to read input csv in chunks

--- app/Parser.php
+++ app/Parser.php
@@ -2,19 +2,23 @@
 
 namespace App;
 
+ini_set('memory_limit', '-1');
+
+use function gc_disable;
 use function fopen;
 use function stream_set_read_buffer;
-use function gc_disable;
-use function fgets;
+use function feof;
+use function fread;
 use function strpos;
 use function substr;
 use function fclose;
-use function gc_enable;
 use function str_replace;
 use function fwrite;
 
 final class Parser
 {
+    CONST CHUNK_SIZE = 1048576; // 1 MB
+
     public function parse(string $inputPath, string $outputPath): void
     {
         gc_disable();
@@ -40,31 +44,51 @@ final class Parser
         unset($y, $m, $d, $date);
 
         $fileHandle = fopen($inputPath, 'rb');
-        
-        stream_set_read_buffer($fileHandle, 1 << 27); // 128MB buffer
 
-        while(($line = fgets($fileHandle)) !== false) {
-            $comma = strpos($line, ',');
-            $path = substr($line, 19, $comma - 19);
-            $date = substr($line, $comma + 1, 10);
+        stream_set_read_buffer($fileHandle, 0);
 
-            if(!isset($output[$path])) {
-                $output[$path] = $dates;
+        $leftover = '';
+
+        while(!feof($fileHandle)) {
+            $chunk = $leftover . fread($fileHandle, self::CHUNK_SIZE);
+            $chunkLen = strlen($chunk);
+            $pos = 0;
+
+            while($pos < $chunkLen) {
+                $commaPos = strpos($chunk, ',', $pos);
+                if($commaPos === false) {
+                    break;
+                }
+                // newline should always be 26 characters after the comma
+                if($commaPos + 26 >= $chunkLen) {
+                    break;
+                }
+
+
+                $path = substr($chunk, $pos + 19, $commaPos - ($pos + 19));
+                $date = substr($chunk, $commaPos + 1, 10);
+
+                if(!isset($output[$path])) {
+                    $output[$path] = $dates;
+                }
+
+                $output[$path][$date]++;
+
+                $pos =  $commaPos + 27;
+
             }
 
-            $output[$path][$date]++;
-
+            $leftover = substr($chunk, $pos);
         }
 
         fclose($fileHandle);
-        gc_enable();
-        unset($fileHandle, $line);
+        unset($fileHandle, $dates, $chunk, $chunkLen, $pos, $commaPos, $path, $date, $leftover);
 
 
         $json ="{\n";
         $s = "";
         foreach($output as $path =>$dates) {
-            $json .= $s . '    "' . str_replace('/','\\/', $path) . '"' . ": {\n";
+            $json .= $s . '    "' . str_replace('/', '\\/', $path) . '"' . ": {\n";
             $sd = "";
             foreach($dates as $date => $count) {
                 if($count === 0) continue;
